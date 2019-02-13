2e37302fa4482f08525abda5eb5d4f236275bb0f
Yunus <yunus@fastmail.com>
2019-02-14 00:12:58 +0300

    Commit

--- 5.3 - Методы/methods/src/main.rs
+++ 5.3 - Методы/methods/src/main.rs
@@ -16,7 +16,7 @@ impl Rectangle {
         self.width * self.height
     }
 
-    // Метод, которому можно передвать один аргумент.
+    // Метод, которому можно передавать один аргумент.
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
